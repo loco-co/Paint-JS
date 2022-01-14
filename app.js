eef0253c8ee19efd5ec00d2b0c4c177528b3afd6
loco-co <loco2925@gmail.com>
2022-01-14 18:06:53 +0900

    #2.3 Changing Color

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
 
 canvas.width = 600;
 canvas.height = 400;
 
 ctx.lineWidth = 2.5;
-ctx.strokeStyle = "palegreen";
+ctx.strokeStyle = "#2c2c2c";
 
 let paintingMode = false;
 
@@ -29,7 +30,14 @@ function onMouseMove(event) {
     }
 }
 
+function changeColor(event) {
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+}
+
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", stopPainting);
 canvas.addEventListener("mouseleave", stopPainting);
 canvas.addEventListener("mousemove", onMouseMove);
+
+Array.from(colors).forEach((color) => color.addEventListener("click", changeColor));
